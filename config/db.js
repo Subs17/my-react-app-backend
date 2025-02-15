1ed9dca8eaffe7f4d88886075aa0c858ea059fde
Subs17 <98551288+Subs17@users.noreply.github.com>
2025-02-14 21:30:47 -0400

    Update db.js
    Added connection timeouts and queue limits to database

--- config/db.js
+++ config/db.js
@@ -8,6 +8,10 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     connectionLimit: 10,
+    waitForConnections: true,
+    queueLimit: 0,
+    connectTimeout: 10000, // 10s timeout
+    debug: false,
 });
 
 //Export the pool
