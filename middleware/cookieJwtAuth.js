089405be9a225b736cb2363ad4317be34897a60f
Subs17 <98551288+Subs17@users.noreply.github.com>
2025-02-14 21:32:45 -0400

    Add files via upload

---
+++ middleware/cookieJwtAuth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+exports.cookieJwtAuth = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({ error: 'Access denied, token missing!' });
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch(error){
+        console.error('Invalid token: ', error);
+        res.clearCookie('token');
+        return res.redirect('/');
+    }
+};
