016115dcd761e2185c4176633b160528cdc3c222
Subs17 <98551288+Subs17@users.noreply.github.com>
2025-02-19 18:51:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const userRoutes = require('./routes/userRoutes'); // Import user routes
+const eventRoutes = require('./routes/eventRoutes'); // Import event routes
 const pool = require('./config/db');
 require('dotenv').config();
 
@@ -13,8 +14,8 @@ const app = express();
 
 // Enable CORS
 const corsOptions ={
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Allow only the frontend to access this API
-    methods: ['GET','POST'],
+    origin: process.env.FRONTEND_URL, // Allow only the frontend to access this API
+    methods: ['GET','POST', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'], // Restrict allowed headers
     credentials: true,
 };
@@ -28,15 +29,25 @@ app.use(bodyParser.json()); // Parse JSON bodies (as sent by API clients)
 
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
+app.use('/uploads', express.static('uploads', {
+    maxAge: '1h',
+}));
+
 // Rate limiting
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
+    max: 500, // Limit each IP to 500 requests per windowMs
 });
 app.use(limiter);
 
-app.use('/api/v1', userRoutes); // Use user routes
+app.use((req, res, next) => {
+    console.log("Request received at: ", req.url);
+    // console.log("Cookies received: ", req.signedCookies);
+    next();
+});
 
+app.use('/api/v1', userRoutes); // Use user routes
+app.use('/api/v1', eventRoutes); // Use event routes
 
 // Start server 
 pool.query('SELECT 1')
